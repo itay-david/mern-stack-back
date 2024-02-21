0c489d11b67e5818df19cc276bf0c5eb4bc2993f
Itay David <itayd3104@gmail.com>
2024-02-21 09:06:51 +0200

    lastf

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use("/posts", postRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect('mongodb://127.0.0.1:27017', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
